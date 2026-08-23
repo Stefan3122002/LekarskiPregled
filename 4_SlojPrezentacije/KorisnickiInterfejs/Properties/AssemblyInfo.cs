2e2b9c9bdef84fc26184d32dd31c6ef46779bf56
Stefan Antonić <60527474+Stefan3122002@users.noreply.github.com>
2026-08-23 19:30:40 +0200

    Delete 4_SlojPrezentacije/KorisnickiInterfejs directory

--- 4_SlojPrezentacije/KorisnickiInterfejs/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("KorisnickiInterfejs")]
-[assembly: AssemblyProduct("KorisnickiInterfejs")]
-[assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
